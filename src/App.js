4fc91dd3ec259b7a3eb3b9fcb05dff2165cd2cf2
infomagnum159 <infomagnum159@gmail.com>
2024-10-28 19:08:39 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reaactos Todoses Gamitos Tanafos
         </a>
       </header>
     </div>
